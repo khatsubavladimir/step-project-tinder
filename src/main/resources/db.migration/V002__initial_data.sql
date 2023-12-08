593bb63df0596c3a10b87e1d049b51cb7eb81b8a
khatsubavladimir <144154790+khatsubavladimir@users.noreply.github.com>
2023-12-08 15:34:09 +0200

    dependencies для БД, (#3)
    класи для сетапу БД,
    додатковий код в ServerApp для міграції,
    sql файли для БД

---
+++ src/main/resources/db.migration/V002__initial_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO students (name, photoUrl) VALUES ('Sheldon', 'https://www.vokrug.tv/pic/person/a/7/7/e/a77e7c2fad04c812869cb7fa9bdfd01c.jpeg');
+INSERT INTO students (name, photoUrl) VALUES ('Emi', 'https://prm.ua/wp-content/uploads/2018/03/Emi-Fara-Fauler.jpeg');
+INSERT INTO students (name, photoUrl) VALUES ('Penni', 'https://today.ua/wp-content/uploads/2022/09/2_EH_CHP_200220The-Big-Bang-Theory_5680JPG.jpg');
