f982c13862320151f3d0b6e329621cb6ad8d5e45
OksanaKorotich <115655363+OksanaKorotich@users.noreply.github.com>
2023-12-08 19:34:17 +0100

    update styles, add url for db (#4)

--- src/main/java/tinder/db/DbConn.java
+++ src/main/java/tinder/db/DbConn.java
@@ -4,15 +4,24 @@ import lombok.SneakyThrows;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbConn {
   @SneakyThrows
   public static Connection create(String url, String username, String password) {
-    return DriverManager.getConnection(url, username, password);
+    try {
+      return DriverManager.getConnection(url, username, password);
+    } catch (SQLException e) {
+      throw new RuntimeException("Failed to create a database connection", e);
+    }
   }
 
   @SneakyThrows
   public static Connection createFromURL(String jdbc_url) {
-    return DriverManager.getConnection(jdbc_url);
+    try {
+      return DriverManager.getConnection(jdbc_url);
+    } catch (SQLException e) {
+      throw new RuntimeException("Failed to create a database connection", e);
+    }
   }
 }
