58c05fb703e4c12ede7975642db85b471213dc68
khatsubavladimir <send4vladimir@gmail.com>
2023-12-09 19:10:12 +0200

    зміни у БД: складається з трьох таблиць users, messages, liked

--- src/main/resources/db.migration/V002__initial_data.sql
+++ src/main/resources/db.migration/V002__initial_data.sql
@@ -1,3 +1,10 @@
-INSERT INTO students (name, photoUrl) VALUES ('Sheldon', 'https://www.vokrug.tv/pic/person/a/7/7/e/a77e7c2fad04c812869cb7fa9bdfd01c.jpeg');
-INSERT INTO students (name, photoUrl) VALUES ('Emi', 'https://prm.ua/wp-content/uploads/2018/03/Emi-Fara-Fauler.jpeg');
-INSERT INTO students (name, photoUrl) VALUES ('Penni', 'https://today.ua/wp-content/uploads/2022/09/2_EH_CHP_200220The-Big-Bang-Theory_5680JPG.jpg');
+INSERT INTO users (username, password_, photoUrl) VALUES ('Sheldon', 'aguiiafu74', 'https://www.vokrug.tv/pic/person/a/7/7/e/a77e7c2fad04c812869cb7fa9bdfd01c.jpeg');
+INSERT INTO users (username, password_, photoUrl) VALUES ('Emi', 'ohg2ug8', 'https://prm.ua/wp-content/uploads/2018/03/Emi-Fara-Fauler.jpeg');
+INSERT INTO users (username, password_, photoUrl) VALUES ('Penni', 'gFoF82Fadp', 'https://today.ua/wp-content/uploads/2022/09/2_EH_CHP_200220The-Big-Bang-Theory_5680JPG.jpg');
+
+INSERT INTO messages (from_id, to_id, message) VALUES (1, 2, 'hi');
+INSERT INTO messages (from_id, to_id, message) VALUES (2, 1, 'hello');
+INSERT INTO messages (from_id, to_id, message) VALUES (1, 3, 'hi');
+
+INSERT INTO liked (user_id, pf_id) VALUES (3, 1);
+INSERT INTO liked (user_id, pf_id) VALUES (3, 2);
