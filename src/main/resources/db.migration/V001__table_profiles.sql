c5889c13b59920c6fc3cc2cbf34dd6caac52da3f
khatsubavladimir <144154790+khatsubavladimir@users.noreply.github.com>
2023-12-10 12:19:49 +0200

    Development kv 2 (#5)
    * зміни у БД:
    складається з трьох таблиць
    users, messages, liked
    
    * Шари ДАО для юзера і роботи з SQL яка його стосується
    
    * Шари ДАО для меседжів і роботи з SQL яка їх стосується
    
    * Шари ДАО для меседжів і роботи з SQL яка їх стосується
    
    * хмл файли змінені, тут вже не знаю чому, сама Idea їх змінює
    
    * перейменував getAll() в getAllProfiles для того щоб легше відрізняти його від getAllUsers()

--- src/main/resources/db.migration/V001__table_profiles.sql
+++ src/main/resources/db.migration/V001__table_profiles.sql
@@ -1,6 +1,21 @@
-create table profiles (
-    id int auto_increment primary key,
-    name varchar not null,
-    photoUrl varchar not null
+create table users (
+                       user_id int auto_increment primary key,
+                       username varchar(20) not null,
+                       password_ varchar(20) not null,
+                       photoUrl varchar(200) not null
 );
-
+create table messages(
+                         mg_id int auto_increment primary key,
+                         from_id int not null,
+                         to_id int not null,
+                         message varchar(200),
+                         constraint from_user foreign key (from_id) references users(user_id),
+                         constraint to_user foreign key (to_id) references users(user_id)
+);
+create table liked(
+                      lk_id int auto_increment primary key,
+                      user_id int not null,
+                      pf_id int not null,
+                      constraint liked_user foreign key (user_id) references users(user_id),
+                      constraint liked_pf foreign key (pf_id) references users(user_id)
+);
