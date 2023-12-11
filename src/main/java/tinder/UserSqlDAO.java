d0ef0b4456690da20df97585c940a7546d9d0494
khatsubavladimir <send4vladimir@gmail.com>
2023-12-11 19:28:59 +0200

    виправлені старі методи і sql запити, створені нові методи, частина функціоналу перенесена повністю у сервіси

--- src/main/java/tinder/UserSqlDAO.java
+++ src/main/java/tinder/UserSqlDAO.java
@@ -3,7 +3,7 @@ package tinder;
 import java.sql.*;
 import java.util.*;
 
-public class UserSqlDAO implements DAO<Profile>{
+public class UserSqlDAO implements DAO<User> {
     public Connection conn;
     public int currentProfileID;
     public UserSqlDAO(Connection conn) {
@@ -11,7 +11,7 @@ public class UserSqlDAO implements DAO<Profile>{
         this.currentProfileID = 0;
     }
 
-    private Iterable<User> resultSetToIterable(ResultSet rs) {
+    Iterable<User> resultSetToIterable(ResultSet rs) {
         return () -> new Iterator<>() {
             @Override
             public boolean hasNext() {
@@ -26,7 +26,7 @@ public class UserSqlDAO implements DAO<Profile>{
             public User next() {
                 try {
                     return new User(
-                            rs.getString("name"),
+                            rs.getString("username"),
                             rs.getString("photoUrl"),
                             rs.getInt("user_id"),
                             rs.getString("password_")
@@ -37,58 +37,48 @@ public class UserSqlDAO implements DAO<Profile>{
             }
         };
     }
-
+    @Override
+    public void add(User user){
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("INSERT INTO users (username, password_, photoUrl) VALUES (?,?,?);");
+            st.setString(1, user.getName());
+            st.setString(2, user.getPassword());
+            st.setString(3, user.getPhotoUrl());
+            st.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
-    public List<Profile> getAllProfiles() {
+    public User getByID(int id) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("SELECT * from users where user_id = ?;");
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+
+            ArrayList<User> list = new ArrayList<>();
+            resultSetToIterable(rs).forEach(x -> list.add(x));
+
+            return list.get(0);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<User> getAll() {
         PreparedStatement st = null;
         try {
             st = conn.prepareStatement("SELECT * from users;");
             ResultSet rs = st.executeQuery();
 
-            ArrayList<Profile> list = new ArrayList<>();
-            resultSetToIterable(rs).forEach(x -> list.add(new Profile(x.getName(), x.getPhotoUrl())));
+            ArrayList<User> list = new ArrayList<>();
+            resultSetToIterable(rs).forEach(x -> list.add(x));
 
             return list;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public void addProfile(Profile item){
-        // профіль це частина юзера - він не зберігається окремо в БД
-    }
-
-    @Override
-    public Profile getNextProfile() {
-        //щоб дізнатись кількість елементів в таблиці доводиться доставати її усю, це погано
-        if (currentProfileID < getAllProfiles().toArray().length) currentProfileID += 1;
-        else currentProfileID = 1;
-
-        PreparedStatement st = null;
-        try {
-            st = conn.prepareStatement("SELECT * from users where id = ?;");
-            st.setInt(1, currentProfileID);
-            ResultSet rs = st.executeQuery();
-
-            ArrayList<User> list = new ArrayList<>();
-            resultSetToIterable(rs).forEach(list::add);
-
-            return new Profile(list.get(0).getName(), list.get(0).getPhotoUrl());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void addUser(User user){
-        PreparedStatement st = null;
-        try {
-            st = conn.prepareStatement("INSERT INTO users (name, password_, photoUrl) VALUES (?,?,?);");
-            st.setString(1, user.getName());
-            st.setString(2, user.getPassword());
-            st.setString(3, user.getPhotoUrl());
-            st.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
