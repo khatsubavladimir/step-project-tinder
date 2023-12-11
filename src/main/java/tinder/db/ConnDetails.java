4544700c1a19e8b7af804571a72558bc0ff5b44f
khatsubavladimir <send4vladimir@gmail.com>
2023-12-11 19:26:03 +0200

    підключено ServerApp до БД

--- src/main/java/tinder/db/ConnDetails.java
+++ src/main/java/tinder/db/ConnDetails.java
@@ -1,7 +1,7 @@
 package tinder.db;
 
 public class ConnDetails {
-  public static final String url = "jdbc:mysql://18.197.50.26:3306/users"; //треба згенерувати
+  public static final String url = "jdbc:mysql://18.197.50.26:3306/users";
   public static final String username = "steptinder";
-  public static final String password = "zaq1xsw2";
+  public static final String password = "zaq1xsw2%F";
 }
