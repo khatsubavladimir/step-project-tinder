5960c00714745b2b8ce1baf2e4545aadef1d73df
khatsubavladimir <send4vladimir@gmail.com>
2024-01-08 18:55:50 +0200

    три нових профіля перенесені в БД

--- src/main/resources/db/migration/V002__initial_data.sql
+++ src/main/resources/db/migration/V002__initial_data.sql
@@ -1,6 +1,9 @@
 INSERT INTO users (username, password_, photoUrl) VALUES ('Sheldon', 'aguiiafu74', 'https://www.vokrug.tv/pic/person/a/7/7/e/a77e7c2fad04c812869cb7fa9bdfd01c.jpeg');
 INSERT INTO users (username, password_, photoUrl) VALUES ('Emi', 'ohg2ug8', 'https://prm.ua/wp-content/uploads/2018/03/Emi-Fara-Fauler.jpeg');
 INSERT INTO users (username, password_, photoUrl) VALUES ('Penni', 'gFoF82Fadp', 'https://today.ua/wp-content/uploads/2022/09/2_EH_CHP_200220The-Big-Bang-Theory_5680JPG.jpg');
+INSERT INTO users (username, password_, photoUrl) VALUES ('Howard', 'pqp0asdkf', 'https://u.movielib.ru/charpics/1001583061/r/oc6b21nl/charpics-r.jpg');
+INSERT INTO users (username, password_, photoUrl) VALUES ('Leonard', 'l3mndGr2', 'https://u.movielib.ru/charpics/1001583059/r/i91fpwso/charpics-r.jpg');
+INSERT INTO users (username, password_, photoUrl) VALUES ('Rajesh', '3jF9oFg2', 'https://img.gazeta.ru/files3/425/10212425/upload-FtYAvibrsVjnr3ulP8iA-pic905-895x505-83376.jpg');
 
 INSERT INTO messages (from_id, to_id, message) VALUES (1, 2, 'hi');
 INSERT INTO messages (from_id, to_id, message) VALUES (2, 1, 'hello');
